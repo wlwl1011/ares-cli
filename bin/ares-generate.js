9d3b862f6a38c449e1cd9fe15ec1ef440bc8b039
wlwl1011 <minji001011@naver.com>
2024-02-21 14:07:22 +0900

    fix : Fix name err

--- bin/ares-generate.js
+++ bin/ares-generate.js
@@ -211,12 +211,12 @@ function generate() {
             if (!template.type) {
                 return;
             }
-            console.log("in ares-generate.js tmplName",tmplName);
+            console.log("in ares-generate.js tmplName",options.tmplName);
             // query mode, only CLI can approach. API only use props
             if(!options.props.length) {
 
                 /// Make query file for acp_service
-                if (options.query && options.tmplName.match(/(hosted_acp_service)/)) {
+                if (options.query && options.tmplName.match(/acp_service)/)) {
                     const queryConfigFile = getQueryFile(config.profile, 'acpservice');
                     return queryInfo(queryConfigFile).then(function(info) {
                         for (const i in info) {
